093d970459b0e5f4803df8e04a77d1d017d1391f
david vidal <davidvidal44@gmail.com>
2023-10-22 13:00:31 -0700

    created controllers

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -10,15 +10,16 @@ module.exports = bands
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll({ 
-            order: [ [ 'available_start_time', 'ASC' ] ] 
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC' ] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
         })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
     }
 })
-
-
 
 
